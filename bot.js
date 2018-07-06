484234874ccbc15f859e614777fad2f05be22056
ryonadv <40894185+ryonadv@users.noreply.github.com>
2018-07-07 01:22:04 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,7 +17,22 @@ client.on('message', message => {
     }
 });
 
-
+client.on("guildMemberAdd", (member) => {
+  const guild = member.guild;
+  if (!newUsers[guild.id]) newUsers[guild.id] = new Discord.Collection();
+  newUsers[guild.id].set(member.id, member.user);
+​
+  if (newUsers[guild.id].size > 10) {
+    const userlist = newUsers[guild.id].map(u => u.toString()).join(" ");
+    guild.channels.find("name", "general").send("Welcome our new users!\n" + userlist);
+    newUsers[guild.id].clear();
+  }
+});
+​
+client.on("guildMemberRemove", (member) => {
+  const guild = member.guild;
+  if (newUsers[guild.id].has(member.id)) newUsers.delete(member.id);
+});
 
 
 //login
