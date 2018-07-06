9885c67874b2d91fcf340ff7713a0e575c1d9c15
ryonadv <40894185+ryonadv@users.noreply.github.com>
2018-07-06 19:58:15 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -255,69 +255,11 @@ client.on('message', message => {
 //comenzi
 
 
-client.on("message", async message => {
+client.on("message", async message => { 
     
-    if(message.content === '/infoserver') {
+    if(message.content === '/info') {
     const m = await message.channel.send("Ping?");
     m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
-  }
-  
-  if(message.content === '/ryonsay') {
-    const sayMessage = args.join(" ");
-    message.delete().catch(O_o=>{}); 
-    message.channel.send(sayMessage);
-  }
-  
-  if(message.content === '/ryonkick') {
-    if(!message.member.roles.some(r=>["Staff Team"].includes(r.name)) )
-      return message.reply("Nu ai permisiunea!");
-    
-    let member = message.mentions.members.first() || message.guild.members.get(args[0]);
-    if(!member)
-      return message.reply("Mentioneaza un membru.");
-    if(!member.kickable) 
-      return message.reply("Are grad mai mare deca tine, nu ii poti da kick.");
-    
-    let reason = args.slice(1).join(' ');
-    if(!reason) reason = "Fara motiv.";
-    
-    await member.kick(reason)
-      .catch(error => message.reply(`Sorry ${message.author} I couldn't kick because of : ${error}`));
-    message.reply(`${member.user.tag} has been kicked by ${message.author.tag} because: ${reason}`);
-
-  }
-  
-  if(message.content === '/ryonban') {
-
-    if(!message.member.roles.some(r=>["Staff Team"].includes(r.name)) )
-      return message.reply("Nu ai permisiunea!");
-    
-    let member = message.mentions.members.first();
-    if(!member)
-      return message.reply("Mentioneaza un membru.");
-    if(!member.bannable) 
-      return message.reply("Are grad mai mare deca tine, nu ii poti da kick.");
-
-    let reason = args.slice(1).join(' ');
-    if(!reason) reason = "Fara motiv.";
-    
-    await member.ban(reason)
-      .catch(error => message.reply(`Sorry ${message.author} I couldn't ban because of : ${error}`));
-    message.reply(`${member.user.tag} has been banned by ${message.author.tag} because: ${reason}`);
-  }
-  
-  if(message.content === '/ryonpurge') {
-
-    const deleteCount = parseInt(args[0], 10);
-    
-
-    if(!deleteCount || deleteCount < 2 || deleteCount > 100)
-      return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
-    
-
-    const fetched = await message.channel.fetchMessages({limit: deleteCount});
-    message.channel.bulkDelete(fetched)
-      .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
   }
 });
 
