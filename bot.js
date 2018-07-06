688c20ba9b7ccced0685b129e087fb1563b54439
ryonadv <40894185+ryonadv@users.noreply.github.com>
2018-07-06 23:23:44 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+client.user.setActivity('YouTube', { type: 'RyonADV BOT so yey' });
 
 client.on('ready', () => {
     console.log('I am ready!');
