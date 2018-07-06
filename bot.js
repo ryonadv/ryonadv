f8a0e9f445ef0f902ae305aa0b31e6af87a3258f
ryonadv <40894185+ryonadv@users.noreply.github.com>
2018-07-07 01:00:43 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,11 +10,6 @@ client.on('message', message => {
     if (message.content === 'ping') {
     	message.reply('pong');
   	}
-    
-    if (msg.includes('test') || msg.includes('testv2') || msg.includes('testv3')) {
-        message.reply('mesajul tau a fost sters deoarece ai folosit un cuvant sau mai multe cuvinte indecente.');
-
-    }
 });
 
 
