103bce82754f5218a400a02be2eb185f6968e1d6
ryonadv <40894185+ryonadv@users.noreply.github.com>
2018-07-06 17:39:32 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,5 +11,17 @@ client.on('message', message => {
   	}
 });
 
+client.on('message', message => {
+    if (message.content === 'ryon') {
+    	message.reply('RyonADV este un smecher.');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'ryonadv') {
+    	message.reply('RyonADV este un smecher.');
+  	}
+});
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
